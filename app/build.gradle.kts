65499640a8eefec842c9b9c7e3e5c50098fb4e0d
Matías Bravo <matias.bravo15@hotmail.com>
2023-09-23 20:40:38 -0300

    permission management

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
 
     val navVersion = "2.7.1"
 
+    val cameraVersion = "1.2.3"
+
     //NavComponent
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
@@ -68,6 +70,13 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.3.1")
+
+    //Camera X
+    implementation("androidx.camera:camera-core:${cameraVersion}")
+    implementation("androidx.camera:camera-camera2:${cameraVersion}")
+    implementation("androidx.camera:camera-lifecycle:${cameraVersion}")
+    implementation("androidx.camera:camera-view:${cameraVersion}")
+    implementation("androidx.camera:camera-extensions:${cameraVersion}")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
--- app/src/main/java/com/matydev/horoscapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/matydev/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -1,9 +1,13 @@
 package com.matydev.horoscapp.ui.palmistry
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import com.matydev.horoscapp.databinding.FragmentPalmistryBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,8 +15,25 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class PalmistryFragment : Fragment() {
 
+    companion object {
+        private const val CAMERA_PERMISSION = Manifest.permission.CAMERA
+    }
+
     private var _binding: FragmentPalmistryBinding? = null
     private val binding get() = _binding!!
+
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+        isGraneted ->
+            if (isGraneted) {
+                // start camera
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Acepta los permisos para poder disfrutar de una experiencia mágica",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +41,23 @@ class PalmistryFragment : Fragment() {
     ): View? {
         _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // comprobar permiso camera
+        if (checkCameraPermission()) {
+            // tiene permisos
+            // start camera
+        } else {
+            requestPermissionLauncher.launch(CAMERA_PERMISSION)
+        }
+
+    }
+
+    private fun checkCameraPermission(): Boolean {
+        return PermissionChecker.checkPermission(requireContext(), CAMERA_PERMISSION, 0, 0, null
+        )== PermissionChecker.PERMISSION_GRANTED
     }
 
 
