1b026cd9d5e71a05eea3ad2a40158ea419b7971b
Matías Bravo <matias.bravo15@hotmail.com>
2023-09-23 21:02:56 -0300

    palmistry screen completed

--- app/src/main/java/com/matydev/horoscapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/matydev/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -2,11 +2,16 @@ package com.matydev.horoscapp.ui.palmistry
 
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import com.matydev.horoscapp.databinding.FragmentPalmistryBinding
@@ -22,10 +27,11 @@ class PalmistryFragment : Fragment() {
     private var _binding: FragmentPalmistryBinding? = null
     private val binding get() = _binding!!
 
-    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-        isGraneted ->
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGraneted ->
             if (isGraneted) {
                 // start camera
+                startCamera()
             } else {
                 Toast.makeText(
                     requireContext(),
@@ -33,12 +39,12 @@ class PalmistryFragment : Fragment() {
                     Toast.LENGTH_LONG
                 ).show()
             }
-    }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
@@ -49,15 +55,42 @@ class PalmistryFragment : Fragment() {
         if (checkCameraPermission()) {
             // tiene permisos
             // start camera
+            startCamera()
         } else {
             requestPermissionLauncher.launch(CAMERA_PERMISSION)
         }
 
     }
 
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview
+                .Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            } catch (e: Exception) {
+                Log.e("Maty", "Error camera: ${e.message}")
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
     private fun checkCameraPermission(): Boolean {
-        return PermissionChecker.checkPermission(requireContext(), CAMERA_PERMISSION, 0, 0, null
-        )== PermissionChecker.PERMISSION_GRANTED
+        return PermissionChecker.checkPermission(
+            requireContext(), CAMERA_PERMISSION, 0, 0, null
+        ) == PermissionChecker.PERMISSION_GRANTED
     }
 
 
